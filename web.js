5783cd8a71b7f05034336fbae05959393f21986c
Mourad DACHROAUI <mourad.dachraoui@gmail.com>
2013-07-11 00:18:05 +0000

    read 'index.html' from disk and use that data in place of 'Hello World 2\!'

--- web.js
+++ web.js
@@ -1,10 +1,13 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
-});
+  var hello = fs.readFileSync('index.html');
+  var buffer = new Buffer(hello, "utf-8");
+  response.send(buffer.toString());
+}
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
